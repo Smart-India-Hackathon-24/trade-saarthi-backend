5f0b5326399faa11df4d69efe298bf357d8fce4f
Khushil-21 <khushilshah2105@gmail.com>
2024-12-07 17:55:12 +0530

    restricted prefix suffix

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ from fastapi.middleware.cors import CORSMiddleware
 import os
 import uvicorn
 from routes.RestrictedWordsRoutes import restricted_words_router
+from routes.RestrictedPrefixSuffixRoutes import restricted_prefix_suffix_router
 
 app = FastAPI(
     title="Trade Mark Sarthi", description="apis for new paper ", version="1.0.0"
@@ -25,6 +26,7 @@ async def root():
 
 # All Validate Title Routes
 app.include_router(restricted_words_router)
+app.include_router(restricted_prefix_suffix_router)
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 8000))
---
+++ routes/RestrictedPrefixSuffixRoutes.py
@@ -0,0 +1,25 @@
+from fastapi import APIRouter, Query
+from functions.RestrictedPrefixSuffixFunctions import *
+
+
+restricted_prefix_suffix_router = APIRouter(
+    prefix="/restricted_prefix_suffix", tags=["Restricted Prefix Suffix"]
+)
+
+
+@restricted_prefix_suffix_router.get("/get")
+async def restricted_prefix_suffix():
+    return get_restricted_prefix_suffix()
+
+
+@restricted_prefix_suffix_router.post("/check")
+async def check_restricted_prefix_suffix(
+    title: str = Query(..., description="Title to check for restricted prefix suffix")
+):
+    result = check_title_in_restricted_prefix_suffix(title.lower())
+    return {
+        "status": "success",
+        "input_title": title,
+        "isValid": result["isValid"],
+        "invalid_words": result["invalid_words"],
+    }
